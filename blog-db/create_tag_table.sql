640043c5bc1314380d8c676c568615066c46565f
Childy <childy.hit@qq.com>
2022-09-20 00:04:57 +0800

    TBD add mysql files

---
+++ blog-db/create_tag_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `blog`.`tag`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `article_id` bigint(0) NOT NULL,
+  `tag_id` bigint(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `article_id`(`article_id`) USING BTREE,
+  INDEX `tag_id`(`tag_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
